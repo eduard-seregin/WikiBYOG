64719cc1c54d64d7fbd17e2c472d6fa17299d34a
Eduard Seregin <seregine@mail.ee>
2022-10-20 07:05:46 +0300

    initiate

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to WikiBYOG</h1>
+	<h3>Wiki for those building your own guide</h3>
+	<p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
